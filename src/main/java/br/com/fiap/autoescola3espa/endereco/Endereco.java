7edb258248009a9a52bcac58c15a96f7fb255f77
Kayque <kayquecarvalho020@gmail.com>
2026-09-25 13:07:40 -0300

    feat(endereco): cria classe embeddable Endereco e seu respectivo DTO

---
+++ src/main/java/br/com/fiap/autoescola3espa/endereco/Endereco.java
@@ -0,0 +1,30 @@
+package br.com.fiap.autoescola3espa.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+
+    public  Endereco(EnderecoDTO corpo){
+        this.logradouro = corpo.logradouro();
+        this.numero = corpo.numero();
+        this.complemento = corpo.complemento();
+        this.bairro = corpo.bairro();
+        this.cidade = corpo.cidade();
+        this.uf = corpo.uf();
+        this.cep = corpo.cep();
+    }
+}
---
+++ src/main/java/br/com/fiap/autoescola3espa/endereco/EnderecoDTO.java
@@ -0,0 +1,11 @@
+package br.com.fiap.autoescola3espa.endereco;
+
+public record EnderecoDTO (
+    String logradouro,
+    String numero,
+    String complemento,
+    String bairro,
+    String cidade,
+    String uf,
+    String cep) {
+}
