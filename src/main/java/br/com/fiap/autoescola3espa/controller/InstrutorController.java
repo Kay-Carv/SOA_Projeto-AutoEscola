c8e58012ec6aa064b18b74aeb59f46fc94ec4025
Kayque <kayquecarvalho020@gmail.com>
2026-09-25 13:09:31 -0300

    feat(instrutor): implementa persistência de dados no endpoint de cadastro

--- src/main/java/br/com/fiap/autoescola3espa/controller/InstrutorController.java
+++ src/main/java/br/com/fiap/autoescola3espa/controller/InstrutorController.java
@@ -1,6 +1,9 @@
 package br.com.fiap.autoescola3espa.controller;
 
-import org.springframework.http.ResponseEntity;
+import br.com.fiap.autoescola3espa.instrutor.Instrutor;
+import br.com.fiap.autoescola3espa.instrutor.InstrutorDTO;
+import br.com.fiap.autoescola3espa.instrutor.InstrutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/instrutor")
 //Receber informações de fora
 public class InstrutorController {
+    @Autowired
+    private InstrutorRepository repository;
+
     @PostMapping
-    public void cadastrarInstrutor(@RequestBody String corpo) {
-        System.out.println(corpo);
+    public void cadastrarInstrutor(@RequestBody InstrutorDTO corpo) {
+        Instrutor instrutor = new Instrutor(corpo);
+        repository.save(instrutor); //gravar dentro do banco
+//        System.out.println(corpo);
     }
 }
